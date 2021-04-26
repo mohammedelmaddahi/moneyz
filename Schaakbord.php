6a7502e4a762556813d81f66025d50ae51b3d2a5
mohammed <2108947@talnet.nl>
2021-04-26 23:08:56 +0200

    Schaakbord.php

---
+++ Schaakbord.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+
+	<head>
+		<title></title>
+
+		</head>
+
+	<body>
+		<table width="400px" cellspacing="0px" border= "1px">
+
+<?php 
+for( $row=1; $row<=8; $row++) 
+			{ 
+
+			echo "<tr>";
+			for($col=1;$col<=8;$col++)
+			 {
+			$total=$row+$col;
+			if ($total%2==0)
+				{
+				echo "<td height= 35px width =30px bgcolor=#FFFFFF></td>";
+								}else{
+				echo "<td height= 35px width =30px bgcolor=#000000></td>";
+							}
+					}
+						echo "</tr>";
+					}
+
+				?>
+		</table>
+	</body>
+</html>
+
